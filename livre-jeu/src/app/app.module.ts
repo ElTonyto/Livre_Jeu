7d3ca0b4e7cf0a8696944cf87a77ae7ff21c1244
Pedrero Tony <tony.pedrero@etu.u-bordeaux.fr>
2019-11-06 09:31:23 +0100

    add page and button components

--- livre-jeu/src/app/app.module.ts
+++ livre-jeu/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
